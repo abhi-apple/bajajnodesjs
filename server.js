646c7d5c2b4d3eba3928c5dfcbbe544d2e904c64
Abhinay Prakash Reddy <abhinayprakashreddy@Abhinays-MacBook-Air.local>
2023-09-19 16:34:48 +0530

    Input validation and exception handeled

--- server.js
+++ server.js
@@ -8,17 +8,25 @@ app.post("/bfhl", (req, res) => {
     const requestData = req.body.data;
 
     if (!requestData || !Array.isArray(requestData)) {
-        return res.status(400).json({ is_success: false, error: "Invalid data format" });
+        return res.status(400).json({ is_success: false, error: "Tha data format is Invalid" });
     }
 
+    const numbers = requestData.filter(item => !isNaN(item));
+    const singleCharAlphabets = requestData.filter(item => typeof item === 'string' && item.length === 1 && isNaN(item));
+
+    if (requestData.some(item => typeof item === 'string' && item.length !== 1 && isNaN(item))) {
+        return res.status(400).json({ is_success: false, error: "Tha data format is Invalid" });
+    }
+    const alph=[...singleCharAlphabets]
     const userResponse = {
         is_success: true,
         user_id: "abhinay_prakash_reddy_01122002",
         email: "gujjala.abhinay2020@vitbhopal.ac.in",
         roll_number: "20BCE10928",
-        numbers: requestData.filter(item => !isNaN(item)),
-        alphabets: requestData.filter(item => isNaN(item)),
-        highest_alphabet: [requestData.filter(item => isNaN(item)).sort()[requestData.filter(item => isNaN(item)).length - 1]]
+        numbers,
+        alphabets: alph,
+        highest_alphabet : singleCharAlphabets.length > 0 ? [singleCharAlphabets.sort()[singleCharAlphabets.length - 1]] : []
+
     };
 
     res.json(userResponse);
@@ -30,6 +38,10 @@ app.get("/bfhl", (req, res) => {
     };
 
     res.status(200).json(responseData);
+});
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello, this is your root URL!");
 });
 
 const port = 3000;
